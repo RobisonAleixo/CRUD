39f1dc2b1a7c11639d6f51e7e88f2ce6b4a59b93
Robison Aleixo <robison.wilson@gmail.com>
2020-04-30 16:49:41 -0300

    CRUD: Backend e frontend finalizado

---
+++ frontend/src/components/template/Nav.jsx
@@ -0,0 +1,11 @@
+import './Nav.css';
+import React from 'react';
+import NavItem from './NavItem';
+
+export default props =>
+    <aside className="menu-area">
+        <nav className="menu">
+            <NavItem link="/" icon="home" label="Início"/>
+            <NavItem link="/users" icon="user" label="Users"/>
+        </nav>
+    </aside>
